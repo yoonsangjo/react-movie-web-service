96b82efe531ed2e0cc4d8654f5a959cb1ef3fd6b
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2024-02-19 17:12:28 +0900

    ##  useEffect, useCallback 경고메세지 처리

--- react/my-app/src/routes/Detail.js
+++ react/my-app/src/routes/Detail.js
@@ -1,26 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Movie from '../components/Movie';
 
 const Detail = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
 
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(
         'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230101'
       )
     ).json();
-    const movie = json.boxOfficeResult.dailyBoxOfficeList.filter((item) => item.rnum === `${id}`);
-    setMovie(movie[0]);
+
+    const selectMovie = json.boxOfficeResult.dailyBoxOfficeList.filter(
+      (item) => item.rnum === `${id}`
+    );
+
+    setMovie(selectMovie[0]);
     setLoading(false);
-  };
+  }, [id]);
 
   useEffect(() => {
     getMovie();
-  }, []);
+  }, [getMovie]);
 
   return (
     <div>
